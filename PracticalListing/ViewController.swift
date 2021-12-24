0234894633ee0acd8847450d7fae4300e6d4add1
joacotome24 <joaco.tome@gmail.com>
2021-12-24 20:38:52 -0300

    Initial Commit

---
+++ PracticalListing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticalListing
+//
+//  Created by Joaquin Tome on 24/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
